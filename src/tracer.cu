c5b27cf0f557c42e641c7865682229738e24e517
SIDD017 <siddhartha.jejurkar5@gmail.com>
2024-08-03 16:08:29 -0400

    Add support for metal and dielectric materials

--- src/tracer.cu
+++ src/tracer.cu
@@ -82,14 +82,16 @@ __device__ uint32_t rgb_to_uint_32(const vec3& col) {
 __global__ void create_world(hitable **d_list, hitable **d_world, camera **d_camera) {
     if (threadIdx.x == 0 && blockIdx.x == 0) {
         d_list[0] = new sphere(vec3(0,0,-1), 0.5,
-                               new lambertian(vec3(0.8, 0.3, 0.3)));
+                                new lambertian(vec3(0.1, 0.2, 0.5)));
         d_list[1] = new sphere(vec3(0,-100.5,-1), 100,
-                               new lambertian(vec3(0.8, 0.8, 0.0)));
+                                new lambertian(vec3(0.8, 0.8, 0.0)));
         d_list[2] = new sphere(vec3(1,0,-1), 0.5,
-                               new metal(vec3(0.8, 0.6, 0.2), 1.0));
+                                new metal(vec3(0.8, 0.6, 0.2), 0.0));
         d_list[3] = new sphere(vec3(-1,0,-1), 0.5,
-                               new metal(vec3(0.8, 0.8, 0.8), 0.3));
-        *d_world  = new hitable_list(d_list,4);
+                                 new dielectric(1.5));
+        d_list[4] = new sphere(vec3(-1,0,-1), -0.45,
+                                 new dielectric(1.5));
+        *d_world = new hitable_list(d_list,5);
         *d_camera = new camera();
     }
 }
@@ -126,7 +128,7 @@ void render(int max_x, int max_y, int ns, vec3 *fb, uint32_t* dev_ptr, camera **
 }
 
 __global__ void free_world(hitable **d_list, hitable **d_world, camera **d_camera) {
-    for(int i=0; i < 4; i++) {
+     for(int i=0; i < 5; i++) {
         delete ((sphere *)d_list[i])->mat_ptr;
         delete d_list[i];
     }
@@ -147,7 +149,7 @@ void Tracer::draw(int tx, int ty, cudaGraphicsResource_t resource) {
     curandState *d_rand_state;
     CHECK_CUDA_ERRORS(cudaMalloc((void **)&d_rand_state, num_pixels*sizeof(curandState)));
     hitable **d_list;
-    CHECK_CUDA_ERRORS(cudaMalloc((void **)&d_list, 4*sizeof(hitable *)));
+    CHECK_CUDA_ERRORS(cudaMalloc((void **)&d_list, 5*sizeof(hitable *)));
     hitable **d_world;
     CHECK_CUDA_ERRORS(cudaMalloc((void **)&d_world, sizeof(hitable *)));
     camera **d_camera;
